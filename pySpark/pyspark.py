5889705a8e9571d3b7e7425062923d2e83f96b08
Shubham Gupta <48982265+shubham1881@users.noreply.github.com>
2024-09-15 12:55:45 +0530

    Update pyspark.py

--- pySpark/pyspark.py
+++ pySpark/pyspark.py
@@ -1,1 +1,19 @@
 
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("First pyspark code") \
+    .getOrCreate()
+
+data = [
+    ("Alice", 25, "New York"),
+    ("Bob", 30, "London"),
+    ("Charlie", 22, "Paris"),
+    ("David", 35, "Tokyo")
+]
+
+columns = ["name", "age", "city"]
+df = spark.createDataFrame(data, columns)
+df.show()
+spark.stop()
